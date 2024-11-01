6cf07cdb45dd679523df4f6e0a64ab833000eac8
Diego <db_elfla10@outlook.es>
2024-11-01 12:39:27 -0400

    Expand user schema, Update the createUser controller, Create the login controller

--- controllers/users.js
+++ controllers/users.js
@@ -17,18 +17,46 @@ const getUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
+  const { name, avatar, email, password } = req.body;
 
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
+  return User.findOne({ email }).then((user) => {
+    if (user) {
+      return res.status(409).send({ message: "This email already exists" });
+    }
+    return User.create({ name, avatar, email, password })
+      .then((user) => res.status(201).send(user))
+      .catch((err) => {
+        console.error(err);
+        if (err.name === "ValidationError") {
+          return res.status(badRequest).send({ message: err.message });
+        }
+        return res
+          .status(internalServerError)
+          .send({ message: "An error has ocurred to the server" });
+      });
+  });
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email | password) {
+    return res
+      .status(badRequest)
+      .send({ message: "Email and password fields are required" });
+  }
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: "7d",
+      });
+      return res.status(200).send({ token });
+    })
     .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(badRequest).send({ message: err.message });
+      if (err.message === "Incorrect email or password") {
+        return res.status(401).send({ message: "Incorrect email or password" });
       }
-      return res
-        .status(internalServerError)
-        .send({ message: "An error has ocurred to the server" });
     });
 };
 
@@ -51,4 +79,4 @@ const getUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, createUser, getUser };
+module.exports = { getUsers, createUser, getUser, login };
